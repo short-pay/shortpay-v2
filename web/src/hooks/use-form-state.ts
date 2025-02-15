8dbc7401c84a99b420e3936ab055fcf9f0e02c43
Theo Furtado Mauricio <58632127+theofurtado05@users.noreply.github.com>
2025-02-14 23:27:23 -0300

    feat: commented unused import

--- web/src/hooks/use-form-state.ts
+++ web/src/hooks/use-form-state.ts
@@ -1,5 +1,5 @@
 import { FormEvent, useState, useTransition } from 'react'
-import { requestFormReset } from 'react-dom'
+// import { requestFormReset } from 'react-dom'
 
 interface FormState {
   success: boolean
